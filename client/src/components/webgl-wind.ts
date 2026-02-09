68d1fceb22d442101e812a147816b5e3c4d26b42
EVILCREATIVES <unleash@evilcreatives.com>
2026-02-09 21:40:25 +0000

    Fix wind animation: set correct alpha in WebGL screen shader

--- client/src/components/webgl-wind.ts
+++ client/src/components/webgl-wind.ts
@@ -133,7 +133,7 @@ const FRAG_SCREEN = `
   varying vec2 v_tex_pos;
   void main() {
     vec4 color = texture2D(u_screen, vec2(v_tex_pos.x, 1.0 - v_tex_pos.y));
-    gl_FragColor = vec4(color.rgb * u_opacity, 0.0);
+    gl_FragColor = vec4(color.rgb * u_opacity, color.a * u_opacity);
   }
 `;
 
